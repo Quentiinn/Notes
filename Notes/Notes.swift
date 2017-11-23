40179c567eeff6cf7cccac205604ae48297750bc
Quentin Courvoisier <quentiin-25@live.fr>
2017-11-23 15:13:08 +0100

    affichage liste

--- Notes/Notes.swift
+++ Notes/Notes.swift
@@ -11,16 +11,14 @@ import Foundation
 class Notes{
     var title: String
     var date: Date
-    var hour: Date
     var content: String?
     var latitude: Float
     var longitude: Float
     
     
-    init(title: String, date: Date, hour: Date, content: String, latitude: Float, longitude: Float) {
+    init(title: String, date: Date, content: String, latitude: Float, longitude: Float) {
         self.title = title
         self.date = date
-        self.hour = hour
         self.content = content
         self.latitude = latitude
         self.longitude = longitude
