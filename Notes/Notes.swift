77497584e2ec69c681efaaa2d1e9ea9d756f9052
Quentin Courvoisier <quentiin-25@live.fr>
2017-11-23 14:47:00 +0100

    first commit

---
+++ Notes/Notes.swift
@@ -0,0 +1,28 @@
+//
+//  Notes.swift
+//  Notes
+//
+//  Created by  on 23/11/2017.
+//  Copyright © 2017 IUT. All rights reserved.
+//
+
+import Foundation
+
+class Notes{
+    var title: String
+    var date: Date
+    var hour: Date
+    var content: String?
+    var latitude: Float
+    var longitude: Float
+    
+    
+    init(title: String, date: Date, hour: Date, content: String, latitude: Float, longitude: Float) {
+        self.title = title
+        self.date = date
+        self.hour = hour
+        self.content = content
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
