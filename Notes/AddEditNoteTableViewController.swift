75f9735e09cd18d752e56d596bcb121b447c9ef1
Quentin Courvoisier <quentiin-25@live.fr>
2017-11-23 15:43:58 +0100

    suppression et deplacement note

--- Notes/AddEditNoteTableViewController.swift
+++ Notes/AddEditNoteTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  NotesTableViewController.swift
+//  AddEditNoteTableViewController.swift
 //  Notes
 //
 //  Created by quentin courvoisier on 23/11/2017.
@@ -8,17 +8,10 @@
 
 import UIKit
 
-class NotesTableViewController: UITableViewController {
-    var notes: [Notes] = [
-        Notes(title:"Test",date:Date(),content:"content test", latitude:45.4, longitude:7),
-        Notes(title:"Test numero 2",date:Date(),content:"content test testtest", latitude:45.4, longitude:7)
-    ]
+class AddEditNoteTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        navigationItem.leftBarButtonItem = editButtonItem
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -36,59 +29,50 @@ class NotesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return notes.count
+        return 0
     }
 
-    
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NotesCell", for: indexPath)
-        let note = notes[indexPath.row]
-        cell.textLabel?.text = note.title
-        let formatter = DateFormatter()
-        // initially set the format based on your datepicker date
-        formatter.dateFormat = "dd/MM/yyyy HH:mm"
-        
-        let myString = formatter.string(from: note.date)
-        cell.detailTextLabel?.text = myString
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
         // Configure the cell...
 
         return cell
     }
-    
+    */
 
-    
+    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
- 
+    */
 
-    
+    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            notes.remove(at: indexPath.row)
+            // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    
+    */
 
-    
+    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-        let movedNote = notes.remove(at: fromIndexPath.row)
-        notes.insert(movedNote, at: to.row)
-        tableView.reloadData()
+
     }
- 
+    */
 
     /*
     // Override to support conditional rearranging of the table view.
